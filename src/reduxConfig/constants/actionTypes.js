6b3fc5a5d9b18a0c12b4f6f26d67b22d8fffc158
Pawel Maryanov <pawelmareyn@gmail.com>
2017-11-11 11:13:12 +0300

    Clean up

---
+++ src/reduxConfig/constants/actionTypes.js
@@ -0,0 +1,18 @@
+export default {
+  app: {
+    setTest: 'APP/SET_TEST',
+  },
+  keys: {
+    setIsFileGenerated: 'KEYS/SET_IS_FILE_GENERATED',
+  },
+  currency: {
+    async: {
+      getLatestRatesRequest: 'CURRENCY/ASYNC/GET_LATEST_RATES_REQUEST',
+      getLatestRatesSuccess: 'CURRENCY/ASYNC/GET_LATEST_RATES_SUCCESS',
+      getLatestRatesFail: 'CURRENCY/ASYNC/GET_LATEST_RATES_FAIL',
+    }
+  },
+  user: {
+    setKeys: 'USER/SET_KEYS',
+  },
+}
