c4b92aa013937ea0af986bbd1267f5cfe777fe35
Pawel Maryanov <pawelmareyn@gmail.com>
2017-11-12 12:26:30 +0300

    Update App container

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { setTest } from 'reduxConfig/actions/app';
 import { getLatestRates } from 'reduxConfig/actions/currency';
 import { Tabs, Tab } from 'material-ui/Tabs';
+import Auth from 'containers/Login/Auth';
+import Registration from 'containers/Login/Registration';
 import ApiResponse from 'components/ApiResponse';
 
 // Styles
@@ -20,56 +22,20 @@ class App extends Component {
   render() {
     return (
       <AppContainer>
-        <h2>Wallet app</h2>
-
+        <TabsExampleSimple />
       </AppContainer>
     )
   };
 
-  handleClick(value) {
-    // set some state in the reduxConfig store
-    this.props.dispatch(setTest(value));
-    // make an API request
-    this.loadLatestRates();
-  };
-
-  loadLatestRates() {
-    this.props.dispatch(getLatestRates());
-  };
 }
 
 const TabsExampleSimple = () => (
   <Tabs>
-    <Tab label="Item One" >
-      <div>
-        <h2>Tab One</h2>
-        <p>
-          This is an example tab.
-        </p>
-        <p>
-          You can put any sort of HTML or react component in here. It even keeps the component state!
-        </p>
-      </div>
+    <Tab label="Login" >
+      <Auth />
     </Tab>
-    <Tab label="Item Two" >
-      <div>
-        <h2>Tab Two</h2>
-        <p>
-          This is another example tab.
-        </p>
-      </div>
-    </Tab>
-    <Tab
-      label="onActive"
-      data-route="/home"
-      onActive={() => false}
-    >
-      <div>
-        <h2 >Tab Three</h2>
-        <p>
-          This is a third example tab.
-        </p>
-      </div>
+    <Tab label="Registration" >
+      <Registration />
     </Tab>
   </Tabs>
 );
